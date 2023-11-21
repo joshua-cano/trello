aab599678c0c871522ea0d6721de2c4d46257e5b
Joshua Cano <joshuacano@Joshuas-MBP.attlocal.net>
2023-11-21 07:59:05 -0600

    First commit

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db";
+import { Form } from "./form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title: {board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
